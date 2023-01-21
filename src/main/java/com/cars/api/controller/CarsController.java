2e109d101d36a5ceb144656c32147c9cf60ff7d4
Juliana Meneguete <jumeneguete@gmail.com>
2023-01-21 18:22:07 -0300

    create put method

--- src/main/java/com/cars/api/controller/CarsController.java
+++ src/main/java/com/cars/api/controller/CarsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,20 @@ public class CarsController {
     @PostMapping
     public void createCar(@RequestBody CarDTO req) {
         repository.save(new Car(req));
+    }
+
+    @PutMapping("/{id}")
+    public void updateCarById(@PathVariable Long id, @RequestBody CarDTO req){
+        repository.findById(id).map(car -> {
+            car.setModelo(req.modelo());
+            car.setFabricante(req.fabricante());
+            car.setDataFabricacao((req.dataFabricacao()));
+            car.setValor(req.valor());
+            car.setAnoModelo(req.anoModelo());
+
+            return repository.save(car);
+        });
+
     }
     
     @DeleteMapping("/{id}")
