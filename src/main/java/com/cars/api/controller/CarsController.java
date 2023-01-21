418a48053bdfa0c2e24a2895e4b8bb92fd74b9a2
Juliana Meneguete <jumeneguete@gmail.com>
2023-01-21 18:10:32 -0300

    create delete method

--- src/main/java/com/cars/api/controller/CarsController.java
+++ src/main/java/com/cars/api/controller/CarsController.java
@@ -3,7 +3,9 @@ package com.cars.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,4 +41,8 @@ public class CarsController {
         repository.save(new Car(req));
     }
     
+    @DeleteMapping("/{id}")
+    public void deleteCarById(@PathVariable Long id){
+        repository.deleteById(id);
+    }
 }
