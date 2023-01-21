a2b6ec7e3a4a5ec3ca80412c6c10f7402fbb3a89
Juliana Meneguete <jumeneguete@gmail.com>
2023-01-21 00:20:54 -0300

    create model and repository

--- src/main/java/com/cars/api/controller/CarsController.java
+++ src/main/java/com/cars/api/controller/CarsController.java
@@ -1,22 +1,42 @@
 package com.cars.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cars.api.dto.CarDTO;
+import com.cars.api.model.Car;
+import com.cars.api.repository.CarRepository;
 
 @RestController
 @RequestMapping("/api/cars")
 public class CarsController {
 
+    @Autowired
+    private CarRepository repository;
+
+    // Aula 1 - Semana 2 - Exercicio 2
+    // @PostMapping
+    // public void createCar(@RequestBody CarDTO req) {
+    //     System.out.println(req);
+    //     System.out.println(req.modelo());
+    //     System.out.println(req.dataFabricacao());
+    //     System.out.println(req.fabricante());
+    // }
+
+    @GetMapping
+    public List<Car> getCars() {
+        return repository.findAll();
+    }
+
     @PostMapping
     public void createCar(@RequestBody CarDTO req) {
-        System.out.println(req);
-        System.out.println(req.modelo());
-        System.out.println(req.dataFabricacao());
-        System.out.println(req.fabricante());
+        repository.save(new Car(req));
     }
     
 }
---
+++ src/main/java/com/cars/api/model/Car.java
@@ -0,0 +1,44 @@
+package com.cars.api.model;
+
+import com.cars.api.dto.CarDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Car {
+
+    public Car(CarDTO req) {
+        this.modelo = req.modelo();
+        this.fabricante = req.fabricante();
+        this.dataFabricacao = req.dataFabricacao();
+        this.valor = req.valor();
+        this.anoModelo = req.anoModelo();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String modelo;
+    
+    @Column(length = 100, nullable = false)   
+    private String fabricante;
+    
+    @Column(length = 100, nullable = false)   
+    private String dataFabricacao;
+    
+    @Column(length = 100, nullable = false)   
+    private double valor;
+    
+    @Column(length = 100, nullable = false)   
+    private int anoModelo;    
+}
